4f760100915cc72194fa5b4992e924ae4ae4bff9
Fatih Dedeic <fatihdedeic2@gmail.com>
2025-07-14 15:52:14 -0400

    Changed 4 indents to 2 indents, remved login functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,21 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
-// Login Functionality
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "IoT Readings Data",
-    description: "Web Application designed to display and allow user to interact with data collected by autonomous temp/pressure/humidity recording devices",
+  title: "IoT Readings Data",
+  description: "Web Application designed to display and allow user to interact with data collected by autonomous temp/pressure/humidity recording devices",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body>
-                <Authenticator>
-                    {children}
-                </Authenticator>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
