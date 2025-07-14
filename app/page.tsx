ffe3a0af5f049499170ff01abda44c7d1ff73866
Fatih Dedeic <fatihdedeic2@gmail.com>
2025-07-14 15:08:47 -0400

    Added Logout Funcrtionality -page.tsx Line 12, Line 21, Line 67 -layout.tsx Line 6-7,

--- app/page.tsx
+++ app/page.tsx
@@ -8,54 +8,63 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
+// Login Functionality
+import { useAuthenticator } from "@aws-amplify/ui-react"; 
+
+
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
+    const { signOut } = useAuthenticator();
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    listTodos();
-  }, []);
+    function listTodos() {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        });
+    }
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Weather Today"),
-    });
+    useEffect(() => {
+        listTodos();
+    }, []);
+
+    function createTodo() {
+        client.models.Todo.create({
+            content: window.prompt("Weather Today"),
+        });
     }
 
 
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
 
-  return (
-    <main>
-      <h1>Weather Entries</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-            <li
-                onClick={() => deleteTodo(todo.id)}
-                key={todo.id}>
-                {todo.content}
-            </li>
-        ))}
-      </ul>
-      <div>
-        Entries may be deleted by clicking.
-        <br />
-        <a href="https://www.theweathernetwork.com/en/city/ca/ontario/toronto/current">
-          Compare entries to real weather.
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Weather Entries</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li
+                        onClick={() => deleteTodo(todo.id)}
+                        key={todo.id}>
+                        {todo.content}
+                    </li>
+                ))}
+            </ul>
+            <div>
+                Entries may be deleted by clicking.
+                <br />
+                <a href="https://www.theweathernetwork.com/en/city/ca/ontario/toronto/current">
+                    Compare entries to real weather.
+                </a>
+            </div>
+
+            // Logout Button
+            <button onClick={signOut}>Sign Out</button>
+        </main>
+    );
 }
