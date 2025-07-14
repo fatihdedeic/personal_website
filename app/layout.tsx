616b13df9aca4fc797d7e1af66b174a55d129753
Fatih Dedeic <fatihdedeic2@gmail.com>
2025-07-14 16:11:01 -0400

    Added login functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
+// ----------------------------------------------
+"use client"
+
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "@/amplify_outputs.json";
+// ---------------------------------------------
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +26,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Authenticator>
+          {children}
+        </Authenticator>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
+// --------------------------------------
+import { useAuthenticator } from "@aws-amplify/ui-react";
+// --------------------------------------
 
 
 Amplify.configure(outputs);
@@ -16,6 +19,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
 
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -60,6 +64,7 @@ export default function App() {
         </a>
       </div>
 
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
