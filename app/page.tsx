43bb5584bc650cbd71fdff3c4d13e5258516487e
Fatih Dedeic <fatihdedeic2@gmail.com>
2025-07-14 19:27:45 -0400

    trying to fix login

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ const client = generateClient<Schema>();
 
 export default function App() {
 
-  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -63,8 +62,6 @@ export default function App() {
           Compare entries to real weather.
         </a>
       </div>
-
-      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
